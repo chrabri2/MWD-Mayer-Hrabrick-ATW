067a5189dfb3e7035ceb6968f9b6c41733353992
chrabri2 <78614483+chrabri2@users.noreply.github.com>
2022-06-22 16:55:59 -0400

    Add axios service to PageList.js

---
+++ PageList-Service.js
@@ -0,0 +1,7 @@
+export function getPageList() {
+  const axios = window.axios;
+  return axios.get("pageList.json").then((response) => {
+    console.log(response.data);
+    return response.data;
+  });
+}
--- PageList.js
+++ PageList.js
@@ -5,33 +5,31 @@ import {
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 
 // This will import the page list from service which extracts it from a database
-// import { placeholder1 } from "./Placeholder1.js";
+import { getPageList } from "./PageList.js";
 
 export function PageList({ title, children }) {
   useEffect(() => {
     document.title = title;
   }, [title]);
 
-  // const [pages, setPages] = useState([]);
+  const [pages, setPages] = useState([]);
 
-  // useEffect(() => {
-  //   console.log("render");
-  //   placeholder1().then((data) => {
-  //     setPlaceholder(data);
-  //   });
-  // }, []);
+  useEffect(() => {
+    console.log("render");
+    getPageList().then((data) => {
+      setPages(data);
+    });
+  }, []);
   // Creates an unordered list of the pages on the wiki, linking to each page
   return html`
-    <header>
-      <h3>${title}</h3>
-      <ul>
-        ${placeholder.map(
-          (page) =>
-            html` <li key="${page}">
-              <link href="${page.url}">${page.text}</link>
-            </li>`
-        )}
-      </ul>
-    </header>
+    <h3>${title}</h3>
+    <ul>
+      ${pages.map(
+        (page) =>
+          html` <li key="${page.id}">
+            <link href="${page.url}">${page.urlText}</link>
+          </li>`
+      )}
+    </ul>
   `;
 }
